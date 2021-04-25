631b035f38c9728b6eeed78669cf1b7b63625d66
tarafdar <tarafdar12@gmail.com>
2021-04-25 18:06:07 +0000

    added bram port in template

--- hls4ml/templates/vivado/AIgean/srcs/conv_2d_large_cl_1x1_port.cpp
+++ hls4ml/templates/vivado/AIgean/srcs/conv_2d_large_cl_1x1_port.cpp
@@ -7,16 +7,16 @@
 void conv_2d_large_cl_1x1_port( 
         hls::stream<INPUT_T> input[N_INPUT], 
         hls::stream<LAYER_T> output[N_OUTPUT],
-        model_weightdefault_t WEIGHTS[N_WEIGHTS]
+        model_weightdefault_t weights[N_WEIGHTS]
         ){
 
 #pragma HLS INTERFACE ap_ctrl_none port=return
 #pragma HLS INTERFACE axis both port=input
 #pragma HLS INTERFACE axis both port=output
-
+#pragma HLS INTERFACE bram port=weights
 #pragma HLS DATAFLOW
 
 
-  _conv_2d_large_cl_1x1<INPUT_T, LAYER_T, CONFIG, N_INPUT, N_OUTPUT> (input, output, WEIGHTS, BIASES) ;
+  _conv_2d_large_cl_1x1<INPUT_T, LAYER_T, CONFIG, N_INPUT, N_OUTPUT> (input, output, weights, BIASES) ;
 
 }
